8de9a8696a13008121fcb949797fb1ae24046902
Ayanalima <sam.lia@outlook.fr>
2024-11-27 15:30:55 +0100

    new formulaire js

---
+++ script.js
@@ -0,0 +1,211 @@
+
+// let nom="Queen";
+// let firstname="Samsam";
+// let age=30;
+// let NumeroSecu=27474749372;
+// console.log(nom, firstname, age, NumeroSecu);
+
+// age=40 ;
+// console.log(age);
+ 
+// console.log("Joyeux anniversaire"+" "+firstname+" "+nom); 
+// console.log("Vous avez"+" "+age+" "+"ans"+" "+"aujourd'hui.");
+
+// let note1=70;
+// let note2=87;
+// let note3=46;
+
+// let sommenotes=note1+note2+note3;
+
+// console.log(sommenotes);
+
+// let moyennenotes=sommenotes/3 
+// console.log(moyennenotes);
+
+// let prix=70;
+
+// let Pourcentage=20;
+
+// let resultat= 12;
+
+
+
+// Exercice produit  
+
+// const numeroProduit=45363848476546;
+// const libelleProduit="GriffesDeChat";
+
+// let numProduit1=numeroProduit+"1"
+
+// console.log(numProduit1);
+
+// let username="samsam"      
+// let jour=24
+
+// function salutation (params) {
+    
+// }
+
+
+
+
+// function verifierReussiteExament(noteEtu,noteMinOK) {
+    
+    // if (noteEtu>=noteMinOK) {
+        // console.log("Vous avez réussi l'exmen 😎");
+        
+    // }
+    
+    // else {
+        // console.log("Vous avez échoué");
+        
+    // }
+    
+// }
+
+
+
+// let noteE=16;
+
+// let noteOK=10 ;
+
+// verifierReussiteExament(noteE,noteOK); 
+
+
+
+// function getRandomInt(max) {
+// return Math.floor(Math.random() * max); }
+ 
+
+//   chiffredev=getRandomInt(10);
+
+//   NbEssai=3
+
+// //   while (NbEssai<3) {
+    
+  
+//   let reponseJoueur=prompt("Quel nombre j'ai choisi ? ")
+
+//   if (reponseJoueur==chiffredev) {
+//   console.log( "GG, t'es un boss😎");
+    
+//   }
+
+//   else if (reponseJoueur!=chiffredev){
+//    console.log("Tu me déçois🙄...Recommence")
+    
+//     };
+
+//     console.log(chiffredev);
+    
+    
+//     if (NbEssai==3){
+//     console.log("Dernière chance d'être intelligent")
+//     NbEssai++ }
+
+//     else {
+//         console.log("Perdu, comme tes neurones au sol la réponse est : "+chiffredev);
+        
+//     }
+
+// };
+
+
+
+
+    
+
+// let apprenants=["Abdelkrim",
+//   "Akaël",
+//   "Alle",
+//   "Amina",
+//   "Ayman",
+//   "Ayoub",
+//   "Bilal",
+//   "Brenda",
+//   "Clarisse",
+//   "Haïdar",
+//   "Hicham",
+//   "Iryna",
+//   "Joël",
+//   "Karim",
+//   "Loïc",
+//   "Luna",
+//   "Mathis",
+//   "Mathis Y",
+//   "Matrami",
+//   "Nur",
+//   "Pierre D",
+//   "Pierre",
+//   "Hayden",
+//   "Ryan",
+//   "Sajad",
+//   "Salomé",
+//   "Samantha",
+//   "Tom DD",
+//   "Tom"];
+
+//  // let NbApprenants=apprenants.length;
+
+// //   apprenants.push("Jessica");
+  
+
+// //  let prof=apprenants[NbApprenants];
+
+
+// //   console.log("Il y a " +NbApprenants + " apprenants dans le groupe formé par " +prof);
+
+// apprenants.push(["Jessica","Loïc","Jorris"])
+
+
+// apprenants.forEach(Prenoms => {
+//     console.log(typeof Prenoms+" Bonjour "+ Prenoms);
+
+// });
+
+// // for (let index = 0; index < apprenants.length; index++) {
+// //     console.log("Bonjour "+apprenants[index])
+    
+// // }
+
+// // for (let index = 0; index < 10; index++) {
+// //     console.log("NEIN");
+    
+    
+// // }
+
+
+// // console.log(apprenants);
+
+
+// let chiffreauhasard=Math.floor(Math.random() * 11) ;
+// console.log(chiffreauhasard);
+
+// let Nbtry=3;
+
+// function devineNum() {
+    
+//     while (Nbtry>0) {
+//         let reponseJ=Number(prompt("Quel nombre j'ai choisi ? "));
+//         if (reponseJ===chiffreauhasard) {
+//             alert("GG t'es un boss");
+//             return reponseJ;
+//             break;
+//         }
+//         else {
+//             Nbtry--;
+//             alert("Faux...");
+//             // return reponseJ;
+//         }
+    
+        
+//     }
+//     if (chiffreauhasard!=reponseJ) {
+//         alert("T'as perdu");
+        
+//     }
+
+
+
+// }
+// devineNum();
